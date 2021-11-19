a05e456cf20c1960285f731b6ef56fc2b1ca78a1
Patrycja Trzcińska <patrycja.trzcinska125@gmail.com>
2021-11-19 19:05:31 +0100

    Changed Linear Gradient

--- app/src/Container.js
+++ app/src/Container.js
@@ -5,13 +5,9 @@ import LinearGradient from 'react-native-linear-gradient';
 
 const Containter = ({ children }) => {
   return (
-    // <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} colors={['#222831', '#393E46']} style={styles.image}>
-    <LinearGradient colors={['#222831', '#393E46']} style={styles.image}>
+    <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }} colors={['#141E30', '#243B55']} style={styles.image}>
       <SafeAreaView style={styles.container}>{children}</SafeAreaView>
     </LinearGradient>
-    // <ImageBackground style={styles.image} source={require('./hardCodingDb/img1.jpg')} resizeMode="cover">
-    //   <SafeAreaView style={styles.container}>{children}</SafeAreaView>
-    // </ImageBackground>
   );
 };
 
