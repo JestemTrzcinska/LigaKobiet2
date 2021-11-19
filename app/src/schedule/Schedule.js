799d800f3bafaafb4807cae09d7d8e649e793a99
Patrycja Trzcińska <patrycja.trzcinska125@gmail.com>
2021-11-19 19:30:29 +0100

    Apply dots by month - not all at once

--- app/src/schedule/Schedule.js
+++ app/src/schedule/Schedule.js
@@ -14,6 +14,10 @@ const showDayGame = (date) => {
   });
 };
 
+const getMonthFormat = (monthNumber) => {
+  return monthNumber.toString().length === 1 ? `0${monthNumber + 1}` : monthNumber + 1;
+};
+
 const ekstraliga = { key: 'Ekstraliga', color: 'chartreuse' };
 const liga1 = { key: '1 liga', color: 'red' };
 const liga2 = { key: '2 liga', color: 'white' };
@@ -21,71 +25,86 @@ const liga3 = { key: '3 liga zachodniopomorska', color: 'blue' };
 
 const markedDates = {};
 
-const returnRigthFormatDate = (date) => {
-  return `${date.getFullYear()}-${
-    date.getMonth().toString().length === 1 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
-  }-${date.getDate().toString().length === 1 ? `0${date.getDate()}` : date.getDate()}`;
+const returnRightFormatDate = (date) => {
+  const day = date.getDate().toString().length === 1 ? `0${date.getDate()}` : date.getDate();
+  const month = getMonthFormat(date.getMonth());
+  const year = date.getFullYear();
+  return `${year}-${month}-${day}`;
 };
 
 // set dots in calendar
-games.map((item) => {
-  const rigthFormatDate = returnRigthFormatDate(new Date(item.date));
+const applyDotsByMonth = (lastDayOfMonth) => {
+  var date = new Date(lastDayOfMonth);
+  var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+  var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
 
-  // check if in the markedDates is already the date that the game(item) is
-  if (markedDates[rigthFormatDate]) {
-    const allLeaguesInMarkedDate = Object.entries(markedDates[rigthFormatDate].dots).map((item) => {
-      return item[1].key;
-    });
-    // check if there is not the wanted league
-    if (!allLeaguesInMarkedDate.includes(item.league)) {
-      // if there is not, add league dot to the marked day
+  const thisMonthGames = games.filter((item) => {
+    if (new Date(item.date) > firstDay && new Date(item.date) < lastDay) {
+      return item;
+    }
+  });
+
+  thisMonthGames.map((item) => {
+    const rightFormatDate = returnRightFormatDate(new Date(item.date));
+
+    // check if in the markedDates is already the date that the game(item) is
+    if (markedDates[rightFormatDate]) {
+      const allLeaguesInMarkedDate = Object.entries(markedDates[rightFormatDate].dots).map((item) => {
+        return item[1].key;
+      });
+      // check if there is not the wanted league
+      if (!allLeaguesInMarkedDate.includes(item.league)) {
+        // if there is not, add league dot to the marked day
+        switch (item.league) {
+          case 'Ekstraliga':
+            markedDates[rightFormatDate].dots.push(ekstraliga);
+            break;
+          case '1 liga':
+            markedDates[rightFormatDate].dots.push(liga1);
+            break;
+          case '2 liga':
+            markedDates[rightFormatDate].dots.push(liga2);
+            break;
+          case '3 liga zachodniopomorska':
+            markedDates[rightFormatDate].dots.push(liga3);
+            break;
+          default:
+            break;
+        }
+      }
+    }
+
+    // if in the markedDates is not the date, add it with right league
+    else {
       switch (item.league) {
         case 'Ekstraliga':
-          markedDates[rigthFormatDate].dots.push(ekstraliga);
+          markedDates[rightFormatDate] = { dots: [ekstraliga] };
           break;
         case '1 liga':
-          markedDates[rigthFormatDate].dots.push(liga1);
+          markedDates[rightFormatDate] = { dots: [liga1] };
           break;
         case '2 liga':
-          markedDates[rigthFormatDate].dots.push(liga2);
+          markedDates[rightFormatDate] = { dots: [liga2] };
           break;
         case '3 liga zachodniopomorska':
-          markedDates[rigthFormatDate].dots.push(liga3);
+          markedDates[rightFormatDate] = { dots: [liga3] };
           break;
         default:
           break;
       }
     }
-  }
-
-  // if in the markedDates is not the date, add it with right league
-  else {
-    switch (item.league) {
-      case 'Ekstraliga':
-        markedDates[rigthFormatDate] = { dots: [ekstraliga] };
-        break;
-      case '1 liga':
-        markedDates[rigthFormatDate] = { dots: [liga1] };
-        break;
-      case '2 liga':
-        markedDates[rigthFormatDate] = { dots: [liga2] };
-        break;
-      case '3 liga zachodniopomorska':
-        markedDates[rigthFormatDate] = { dots: [liga3] };
-        break;
-      default:
-        break;
-    }
-  }
-});
+  });
+};
 
 const options = { day: 'numeric', month: 'long', year: 'numeric' };
 
 export const Schedule = ({ navigation, route }) => {
   const [daySelected, setDaySelected] = useState(new Date());
+  const [lastDayOfMonth, setLastDayOfMonth] = useState(new Date());
 
   return (
     <Containter>
+      {applyDotsByMonth(lastDayOfMonth)}
       <Calendar
         theme={{
           backgroundColor: 'transparent',
@@ -99,11 +118,8 @@ export const Schedule = ({ navigation, route }) => {
         onDayPress={(day) => {
           setDaySelected(day.dateString);
         }}
-        onDayLongPress={(day) => {
-          console.log('selected day', day);
-        }}
         onMonthChange={(month) => {
-          console.log('month changed', month);
+          setLastDayOfMonth(month.dateString);
         }}
         firstDay={1}
         disableAllTouchEventsForDisabledDays={true}
