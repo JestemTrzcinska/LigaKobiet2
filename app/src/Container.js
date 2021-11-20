b8e7d25088d7f6548477657e1b4c8f3cde47d33c
Patrycja Trzcińska <patrycja.trzcinska125@gmail.com>
2021-11-20 21:43:53 +0100

    Changed style of StatusBar - white color

--- app/src/Container.js
+++ app/src/Container.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
 const Containter = ({ children }) => {
   return (
     <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }} colors={['#1C0C5B', '#3D2C8D']} style={styles.image}>
+      <StatusBar barStyle="light-content" />
       <SafeAreaView style={styles.container}>{children}</SafeAreaView>
     </LinearGradient>
   );
