b9abbe572fdb1a5936e65ce5a8233c68289f3904
Patrycja Trzcińska <patrycja.trzcinska125@gmail.com>
2021-11-20 22:55:00 +0100

    Server & APIs connected

---
+++ routes/api/clubs.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const route = express.Router();
+
+// @route       GET api/clubs
+// @desc        Test route
+// @access      Public
+route.get("/", (req, res) => res.send("API Running"));
+
+module.exports = route;
--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const connectDb = require("./config/db");
 
 const app = express();
 
+// Connect Database
+connectDb();
+
 app.get("/", (req, res) => res.send("API RUnning"));
+
+// Define routes
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/clubs", require("./routes/api/clubs"));
+app.use("/api/games", require("./routes/api/games"));
+app.use("/api/leagues", require("./routes/api/leagues"));
+app.use("/api/players", require("./routes/api/players"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/seasons", require("./routes/api/seasons"));
+app.use("/api/users", require("./routes/api/users"));
 
 const PORT = process.env.PORT || 5000;
 
